46a7069da63d0ed59a4ee5b9ccdb25c76fde246d
Evans Musomi <evans.musomi@gmail.com>
2020-10-15 06:12:33 +0300

    completes form fundamentals

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,33 +1,69 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React from 'react'
+import React, { useState } from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
 
-  // 🐨 add the onSubmit handler to the <form> below
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmitUsername(event.target.elements.usernameInput.value)
+  }
 
-  // 🐨 replace input's name attribute with id attribute
-  // 🐨 make sure to associate the label to the input. 
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input name="username" type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
   )
+
+  /* extra 1 - Refs
+  const usernameInputRef = React.useRef()
+  const handleSubmit = event => {
+    event.preventDefault()
+    onSubmitUsername(usernameInputRef.current.value)
+  }*/
+
+  /* extra 2 - useState for Errors
+  const [error, setError] = React.useState(null)
+
+  const handleChange = event => {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null: 'Username must be lower case')
+  }
+
+  const errorMessage = error ? (<div role="alert" style={{color:'red'}}>{error}</div>): null
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input ref={usernameInputRef} id="usernameInput" type="text" onChange={handleChange} />
+      </div>
+      {errorMessage}
+      <button type="submit" disabled={Boolean(error)}>Submit</button>
+    </form>
+  )*/
+
+  /* extra 3 - Controlled Forms
+  const [username, setUsername] = useState('')
+  const handleChange = (event) => {
+    setUsername(event.target.value.toLowerCase())
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" onChange={handleChange} value={username} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )*/
 }
 
 function App() {
